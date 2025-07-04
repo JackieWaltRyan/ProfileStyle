e97e36ecb82f5fa1e0b69abe0a73f7b36c22f807
Jackie Ryan <jackiewaltryan@gmail.com>
2025-07-05 00:55:39 +0300

    ProfileStyle

---
+++ ProfileStyle/Interfaces.cs
@@ -0,0 +1,64 @@
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace ProfileStyle;
+
+internal sealed record ProfileStyleConfig {
+    [JsonInclude]
+    public CategoryConfig Avatars { get; set; } = new();
+
+    [JsonInclude]
+    public CategoryConfig AvatarFrames { get; set; } = new();
+
+    [JsonInclude]
+    public CategoryConfig MiniBackgrounds { get; set; } = new();
+
+    [JsonInclude]
+    public CategoryBackgroundConfig Backgrounds { get; set; } = new();
+
+    [JsonInclude]
+    public CategoryConfig SpecialProfiles { get; set; } = new();
+
+    internal record CategoryConfig {
+        [JsonInclude]
+        public bool Enable { get; set; }
+
+        [JsonInclude]
+        public List<int> Items { get; set; } = [];
+
+        [JsonInclude]
+        public uint Timeout { get; set; } = 1;
+
+        [JsonConstructor]
+        public CategoryConfig() { }
+    }
+
+    internal sealed record CategoryBackgroundConfig : CategoryConfig {
+        [JsonInclude]
+        public List<int> Showcases { get; set; } = [];
+
+        [JsonConstructor]
+        public CategoryBackgroundConfig() { }
+    }
+
+    [JsonConstructor]
+    public ProfileStyleConfig() { }
+}
+
+internal sealed record GetCommunityInventoryResponse {
+    [JsonPropertyName("response")]
+    public ResponseData? Response { get; set; }
+
+    internal sealed record ResponseData {
+        [JsonPropertyName("items")]
+        public List<Item>? Items { get; set; }
+
+        internal sealed record Item {
+            [JsonPropertyName("communityitemid")]
+            public int CommunityItemId { get; set; }
+
+            [JsonPropertyName("appid")]
+            public int AppId { get; set; }
+        }
+    }
+}
--- ProfileStyle/ProfileStyle.cs
+++ ProfileStyle/ProfileStyle.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using ArchiSteamFarm.Helpers.Json;
 using ArchiSteamFarm.Plugins.Interfaces;
 using ArchiSteamFarm.Steam;
+using ArchiSteamFarm.Steam.Integration;
+using ArchiSteamFarm.Web.Responses;
 
 namespace ProfileStyle;
 
@@ -14,62 +17,300 @@ internal sealed class ProfileStyle : IGitHubPluginUpdates, IBotModules {
     public string RepositoryName => "JackieWaltRyan/ProfileStyle";
     public Version Version => typeof(ProfileStyle).Assembly.GetName().Version ?? throw new InvalidOperationException(nameof(Version));
 
-    public Timer UserDataRefreshTimer;
+    public Dictionary<string, ProfileStyleConfig> ProfileStyleConfig = new();
+    public Dictionary<string, Dictionary<string, Timer>> ProfileStyleTimers = new();
 
     public Task OnLoaded() => Task.CompletedTask;
 
-    public Task OnBotInitModules(Bot bot, IReadOnlyDictionary<string, JsonElement>? additionalConfigProperties = null) {
-        if (additionalConfigProperties == null) {
-            return Task.CompletedTask;
-        }
+    public async Task OnBotInitModules(Bot bot, IReadOnlyDictionary<string, JsonElement>? additionalConfigProperties = null) {
+        if (additionalConfigProperties != null) {
+            if (ProfileStyleTimers.TryGetValue(bot.BotName, out Dictionary<string, Timer>? dict)) {
+                foreach (KeyValuePair<string, Timer> timers in dict) {
+                    switch (timers.Key) {
+                        case "ChangeAvatar": {
+                            await timers.Value.DisposeAsync().ConfigureAwait(false);
 
-        bool isEnabled = false;
-        uint timeout = 60;
+                            bot.ArchiLogger.LogGenericInfo("ChangeAvatar Dispose.");
 
-        foreach (KeyValuePair<string, JsonElement> configProperty in additionalConfigProperties) {
-            switch (configProperty.Key) {
-                case "EnableProfileStyle" when configProperty.Value.ValueKind is JsonValueKind.True or JsonValueKind.False: {
-                    isEnabled = configProperty.Value.GetBoolean();
-
-                    bot.ArchiLogger.LogGenericInfo($"Enable Profile Style: {isEnabled}");
-
-                    break;
-                }
-
-                case "ProfileStyleTimeout" when configProperty.Value.ValueKind is JsonValueKind.Number: {
-                    timeout = configProperty.Value.ToJsonObject<uint>();
-
-                    bot.ArchiLogger.LogGenericInfo($"Profile Style Timeout: {timeout}");
-
-                    break;
-                }
-
-                case "ProfileStyleSettings": {
-                    FilterConfig? filter = configProperty.Value.ToJsonObject<FilterConfig>();
-
-                    if (filter != null) {
-                        if (filter.Types.Contains("Demo") && filter.IgnoredTypes.Contains("Demo")) {
-                            filter.IgnoredTypes.Remove("Demo");
+                            break;
                         }
 
-                        if (filter.MinDaysOld > 0 && filter.MaxDaysOld == 0) {
-                            filter.MaxDaysOld = filter.MinDaysOld;
+                        case "ChangeAvatarFrame": {
+                            await timers.Value.DisposeAsync().ConfigureAwait(false);
+
+                            bot.ArchiLogger.LogGenericInfo("ChangeAvatarFrame Dispose.");
+
+                            break;
                         }
 
-                        bot.ArchiLogger.LogGenericInfo("Profile Style Settings: " + filter.ToJsonText());
+                        case "ChangeMiniBackground": {
+                            await timers.Value.DisposeAsync().ConfigureAwait(false);
 
-                        filterConfigs.Add(filter);
+                            bot.ArchiLogger.LogGenericInfo("ChangeMiniBackground Dispose.");
+
+                            break;
+                        }
+
+                        case "ChangeBackground": {
+                            await timers.Value.DisposeAsync().ConfigureAwait(false);
+
+                            bot.ArchiLogger.LogGenericInfo("ChangeBackground Dispose.");
+
+                            break;
+                        }
+
+                        case "ChangeSpecialProfile": {
+                            await timers.Value.DisposeAsync().ConfigureAwait(false);
+
+                            bot.ArchiLogger.LogGenericInfo("ChangeSpecialProfile Dispose.");
+
+                            break;
+                        }
                     }
+                }
+            }
 
-                    break;
+            ProfileStyleTimers[bot.BotName] = new Dictionary<string, Timer> {
+                { "ChangeAvatar", new Timer(async e => await ChangeAvatar(bot).ConfigureAwait(false), null, Timeout.Infinite, Timeout.Infinite) },
+                { "ChangeAvatarFrame", new Timer(async e => await ChangeAvatarFrame(bot).ConfigureAwait(false), null, Timeout.Infinite, Timeout.Infinite) },
+                { "ChangeMiniBackground", new Timer(async e => await ChangeMiniBackground(bot).ConfigureAwait(false), null, Timeout.Infinite, Timeout.Infinite) },
+                { "ChangeBackground", new Timer(async e => await ChangeBackground(bot).ConfigureAwait(false), null, Timeout.Infinite, Timeout.Infinite) },
+                { "ChangeSpecialProfile", new Timer(async e => await ChangeSpecialProfile(bot).ConfigureAwait(false), null, Timeout.Infinite, Timeout.Infinite) }
+            };
+
+            ProfileStyleConfig[bot.BotName] = new ProfileStyleConfig();
+
+            foreach (KeyValuePair<string, JsonElement> configProperty in additionalConfigProperties) {
+                switch (configProperty.Key) {
+                    case "ProfileStyleConfig": {
+                        ProfileStyleConfig? config = configProperty.Value.ToJsonObject<ProfileStyleConfig>();
+
+                        if (config != null) {
+                            ProfileStyleConfig[bot.BotName] = config;
+                        }
+
+                        break;
+                    }
+                }
+            }
+
+            ProfileStyleConfig psc = ProfileStyleConfig[bot.BotName];
+
+            if (psc.Avatars.Enable || psc.AvatarFrames.Enable || psc.MiniBackgrounds.Enable || psc.Backgrounds.Enable || psc.SpecialProfiles.Enable) {
+                bot.ArchiLogger.LogGenericInfo($"ProfileStyleConfig: {psc.ToJsonText()}");
+
+                if (psc.Avatars.Enable && (psc.Avatars.Items.Count > 0)) {
+                    ProfileStyleTimers[bot.BotName]["ChangeAvatar"].Change(1, -1);
+                }
+
+                if (psc.AvatarFrames.Enable && (psc.AvatarFrames.Items.Count > 0)) {
+                    ProfileStyleTimers[bot.BotName]["ChangeAvatarFrame"].Change(1, -1);
+                }
+
+                if (psc.MiniBackgrounds.Enable && (psc.MiniBackgrounds.Items.Count > 0)) {
+                    ProfileStyleTimers[bot.BotName]["ChangeMiniBackground"].Change(1, -1);
+                }
+
+                if (psc.Backgrounds.Enable && (psc.Backgrounds.Items.Count > 0)) {
+                    ProfileStyleTimers[bot.BotName]["ChangeBackground"].Change(1, -1);
+                }
+
+                if (psc.SpecialProfiles.Enable && (psc.SpecialProfiles.Items.Count > 0)) {
+                    ProfileStyleTimers[bot.BotName]["ChangeSpecialProfile"].Change(1, -1);
                 }
             }
         }
+    }
 
-        if (isEnabled) {
-            timeout;
+    public async Task ChangeAvatar(Bot bot) {
+        uint timeout = 1;
+
+        if (bot.IsConnectedAndLoggedOn) {
+            int communityitemid = ProfileStyleConfig[bot.BotName].Avatars.Items[RandomNumberGenerator.GetInt32(ProfileStyleConfig[bot.BotName].Avatars.Items.Count)];
+
+            bool response = await bot.ArchiWebHandler.UrlPostWithSession(
+                new Uri("https://api.steampowered.com/IPlayerService/SetAnimatedAvatar/v1/"), data: new Dictionary<string, string>(2) {
+                    { "access_token", bot.AccessToken ?? string.Empty },
+                    { "communityitemid", $"{communityitemid}" }
+                }, session: ArchiWebHandler.ESession.None
+            ).ConfigureAwait(false);
+
+            if (response) {
+                timeout = ProfileStyleConfig[bot.BotName].Avatars.Timeout;
+
+                bot.ArchiLogger.LogGenericInfo($"ID: {communityitemid} | Status: OK | Next run: {DateTime.Now.AddMinutes(timeout):T}");
+            } else {
+                bot.ArchiLogger.LogGenericInfo($"Status: Error | Next run: {DateTime.Now.AddMinutes(timeout):T}");
+            }
+        } else {
+            bot.ArchiLogger.LogGenericInfo($"Status: BotNotConnected | Next run: {DateTime.Now.AddMinutes(timeout):T}");
         }
 
-        return Task.CompletedTask;
+        ProfileStyleTimers[bot.BotName]["ChangeAvatar"].Change(TimeSpan.FromMinutes(timeout), TimeSpan.FromMilliseconds(-1));
+    }
+
+    public async Task ChangeAvatarFrame(Bot bot) {
+        uint timeout = 1;
+
+        if (bot.IsConnectedAndLoggedOn) {
+            int communityitemid = ProfileStyleConfig[bot.BotName].AvatarFrames.Items[RandomNumberGenerator.GetInt32(ProfileStyleConfig[bot.BotName].AvatarFrames.Items.Count)];
+
+            bool response = await bot.ArchiWebHandler.UrlPostWithSession(
+                new Uri("https://api.steampowered.com/IPlayerService/SetAvatarFrame/v1/"), data: new Dictionary<string, string>(2) {
+                    { "access_token", bot.AccessToken ?? string.Empty },
+                    { "communityitemid", $"{communityitemid}" }
+                }, session: ArchiWebHandler.ESession.None
+            ).ConfigureAwait(false);
+
+            if (response) {
+                timeout = ProfileStyleConfig[bot.BotName].AvatarFrames.Timeout;
+
+                bot.ArchiLogger.LogGenericInfo($"ID: {communityitemid} | Status: OK | Next run: {DateTime.Now.AddMinutes(timeout):T}");
+            } else {
+                bot.ArchiLogger.LogGenericInfo($"Status: Error | Next run: {DateTime.Now.AddMinutes(timeout):T}");
+            }
+        } else {
+            bot.ArchiLogger.LogGenericInfo($"Status: BotNotConnected | Next run: {DateTime.Now.AddMinutes(timeout):T}");
+        }
+
+        ProfileStyleTimers[bot.BotName]["ChangeAvatarFrame"].Change(TimeSpan.FromMinutes(timeout), TimeSpan.FromMilliseconds(-1));
+    }
+
+    public async Task ChangeMiniBackground(Bot bot) {
+        uint timeout = 1;
+
+        if (bot.IsConnectedAndLoggedOn) {
+            int communityitemid = ProfileStyleConfig[bot.BotName].MiniBackgrounds.Items[RandomNumberGenerator.GetInt32(ProfileStyleConfig[bot.BotName].MiniBackgrounds.Items.Count)];
+
+            bool response = await bot.ArchiWebHandler.UrlPostWithSession(
+                new Uri("https://api.steampowered.com/IPlayerService/SetMiniProfileBackground/v1/"), data: new Dictionary<string, string>(2) {
+                    { "access_token", bot.AccessToken ?? string.Empty },
+                    { "communityitemid", $"{communityitemid}" }
+                }, session: ArchiWebHandler.ESession.None
+            ).ConfigureAwait(false);
+
+            if (response) {
+                timeout = ProfileStyleConfig[bot.BotName].MiniBackgrounds.Timeout;
+
+                bot.ArchiLogger.LogGenericInfo($"ID: {communityitemid} | Status: OK | Next run: {DateTime.Now.AddMinutes(timeout):T}");
+            } else {
+                bot.ArchiLogger.LogGenericInfo($"Status: Error | Next run: {DateTime.Now.AddMinutes(timeout):T}");
+            }
+        } else {
+            bot.ArchiLogger.LogGenericInfo($"Status: BotNotConnected | Next run: {DateTime.Now.AddMinutes(timeout):T}");
+        }
+
+        ProfileStyleTimers[bot.BotName]["ChangeMiniBackground"].Change(TimeSpan.FromMinutes(timeout), TimeSpan.FromMilliseconds(-1));
+    }
+
+    public async Task ChangeShowcase(Bot bot, int index) {
+        if (bot.IsConnectedAndLoggedOn) {
+            int communityitemid = ProfileStyleConfig[bot.BotName].Backgrounds.Showcases[index];
+
+            bool response = await bot.ArchiWebHandler.UrlPostWithSession(
+                new Uri("https://api.steampowered.com/IPlayerService/SetProfileBackground/v1/"), data: new Dictionary<string, string>(2) {
+                    { "access_token", bot.AccessToken ?? string.Empty },
+                    { "communityitemid", $"{communityitemid}" }
+                }, session: ArchiWebHandler.ESession.None
+            ).ConfigureAwait(false);
+
+            bot.ArchiLogger.LogGenericInfo(response ? $"ID: {communityitemid} | Status: OK" : "Status: Error");
+        } else {
+            bot.ArchiLogger.LogGenericInfo("Status: BotNotConnected");
+        }
+    }
+
+    public async Task ChangeBackground(Bot bot) {
+        uint timeout = 1;
+
+        if (bot.IsConnectedAndLoggedOn) {
+            int random = RandomNumberGenerator.GetInt32(ProfileStyleConfig[bot.BotName].Backgrounds.Items.Count);
+
+            int communityitemid = ProfileStyleConfig[bot.BotName].Backgrounds.Items[random];
+
+            bool response = await bot.ArchiWebHandler.UrlPostWithSession(
+                new Uri("https://api.steampowered.com/IPlayerService/SetProfileBackground/v1/"), data: new Dictionary<string, string>(2) {
+                    { "access_token", bot.AccessToken ?? string.Empty },
+                    { "communityitemid", $"{communityitemid}" }
+                }, session: ArchiWebHandler.ESession.None
+            ).ConfigureAwait(false);
+
+            if (response) {
+                timeout = ProfileStyleConfig[bot.BotName].Backgrounds.Timeout;
+
+                bot.ArchiLogger.LogGenericInfo($"ID: {communityitemid} | Status: OK | Next run: {DateTime.Now.AddMinutes(timeout):T}");
+
+                if (ProfileStyleConfig[bot.BotName].Backgrounds.Showcases.Count >= random + 1) {
+                    await ChangeShowcase(bot, random).ConfigureAwait(false);
+                }
+            } else {
+                bot.ArchiLogger.LogGenericInfo($"Status: Error | Next run: {DateTime.Now.AddMinutes(timeout):T}");
+            }
+        } else {
+            bot.ArchiLogger.LogGenericInfo($"Status: BotNotConnected | Next run: {DateTime.Now.AddMinutes(timeout):T}");
+        }
+
+        ProfileStyleTimers[bot.BotName]["ChangeBackground"].Change(TimeSpan.FromMinutes(timeout), TimeSpan.FromMilliseconds(-1));
+    }
+
+    public async Task ChangeSpecialProfile(Bot bot) {
+        uint timeout = 1;
+
+        if (bot.IsConnectedAndLoggedOn) {
+            ObjectResponse<GetCommunityInventoryResponse>? rawResponse = await bot.ArchiWebHandler.UrlGetToJsonObjectWithSession<GetCommunityInventoryResponse>(new Uri($"https://api.steampowered.com/IQuestService/GetCommunityInventory/v1/?access_token={bot.AccessToken}")).ConfigureAwait(false);
+
+            List<GetCommunityInventoryResponse.ResponseData.Item>? items = rawResponse?.Content?.Response?.Items;
+
+            if (items != null) {
+                int communityitemid = ProfileStyleConfig[bot.BotName].SpecialProfiles.Items[RandomNumberGenerator.GetInt32(ProfileStyleConfig[bot.BotName].SpecialProfiles.Items.Count)];
+
+                GetCommunityInventoryResponse.ResponseData.Item? item = items.Find(x => x.CommunityItemId == communityitemid);
+
+                if (item != null) {
+                    bool response = await bot.ArchiWebHandler.UrlPostWithSession(
+                        new Uri("https://api.steampowered.com/IQuestService/ActivateProfileModifierItem/v1/"), data: new Dictionary<string, string>(4) {
+                            { "access_token", bot.AccessToken ?? string.Empty },
+                            { "appid", $"{item.AppId}" },
+                            { "communityitemid", $"{communityitemid}" },
+                            { "activate", "true" }
+                        }, session: ArchiWebHandler.ESession.None
+                    ).ConfigureAwait(false);
+
+                    if (response) {
+                        timeout = ProfileStyleConfig[bot.BotName].SpecialProfiles.Timeout;
+
+                        bot.ArchiLogger.LogGenericInfo($"ID: {communityitemid} | Status: OK | Next run: {DateTime.Now.AddMinutes(timeout):T}");
+
+                        ProfileStyleConfig psc = ProfileStyleConfig[bot.BotName];
+
+                        if (psc.Avatars.Enable && (psc.Avatars.Items.Count > 0)) {
+                            ProfileStyleTimers[bot.BotName]["ChangeAvatar"].Change(1, -1);
+                        }
+
+                        if (psc.AvatarFrames.Enable && (psc.AvatarFrames.Items.Count > 0)) {
+                            ProfileStyleTimers[bot.BotName]["ChangeAvatarFrame"].Change(1, -1);
+                        }
+
+                        if (psc.MiniBackgrounds.Enable && (psc.MiniBackgrounds.Items.Count > 0)) {
+                            ProfileStyleTimers[bot.BotName]["ChangeMiniBackground"].Change(1, -1);
+                        }
+
+                        if (psc.Backgrounds.Enable && (psc.Backgrounds.Items.Count > 0)) {
+                            ProfileStyleTimers[bot.BotName]["ChangeBackground"].Change(1, -1);
+                        }
+                    } else {
+                        bot.ArchiLogger.LogGenericInfo($"Status: Error | Next run: {DateTime.Now.AddMinutes(timeout):T}");
+                    }
+                } else {
+                    bot.ArchiLogger.LogGenericInfo($"Status: Error | Next run: {DateTime.Now.AddMinutes(timeout):T}");
+                }
+            } else {
+                bot.ArchiLogger.LogGenericInfo($"Status: Error | Next run: {DateTime.Now.AddMinutes(timeout):T}");
+            }
+        } else {
+            bot.ArchiLogger.LogGenericInfo($"Status: BotNotConnected | Next run: {DateTime.Now.AddMinutes(timeout):T}");
+        }
+
+        ProfileStyleTimers[bot.BotName]["ChangeSpecialProfile"].Change(TimeSpan.FromMinutes(timeout), TimeSpan.FromMilliseconds(-1));
     }
 }
